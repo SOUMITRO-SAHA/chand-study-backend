d2a0d8c21fb285a619df89026b707ef4d86e7c69
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-09-25 18:23:19 +0530

    Update Courese Controller Updated

--- models/test.model.js
+++ models/test.model.js
@@ -1,44 +1,44 @@
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/db.config");
-const Section = require("./section.model");
-const { Languages } = require("../utils/Languages");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db.config');
+const Section = require('./section.model');
+const { Languages } = require('../utils/Languages');
 
-const Test = sequelize.define("tests", {
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		autoIncrement: true,
-	},
-	testName: {
-		type: DataTypes.STRING,
-		allowNull: false,
-		required: true,
-	},
-	duration: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-	},
-	language: {
-		type: DataTypes.ENUM(Object.values(Languages)),
-		allowNull: false,
-		defaultValue: Languages.HINDI,
-	},
-	instruction: {
-		type: DataTypes.TEXT,
-		allowNull: true,
-	},
-	totalMarks: {
-		type: DataTypes.INTEGER,
-		allowNull: true,
-	},
-	totalQuestions: {
-		type: DataTypes.INTEGER,
-		allowNull: true,
-	},
-	answers: {
-		type: DataTypes.JSON,
-		allowNull: true,
-	},
+const Test = sequelize.define('tests', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  testName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    required: true,
+  },
+  duration: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  language: {
+    type: DataTypes.ENUM(Object.values(Languages)),
+    allowNull: false,
+    defaultValue: Languages.HINDI,
+  },
+  instruction: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  totalMarks: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  totalQuestions: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  answers: {
+    type: DataTypes.JSON,
+    allowNull: true,
+  },
 });
 
 // Relationship between Test and Section
